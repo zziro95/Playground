3b568d0197e4b7d21441ef8131d593d19754a5be
zziro95 <opers7878@gmail.com>
2023-04-22 14:23:55 +0900

    Initial Commit

---
+++ MyPlayground/MyPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlaygroundApp.swift
+//  MyPlayground
+//
+//  Created by Jinho Choi on 2023/04/22.
+//
+
+import SwiftUI
+
+@main
+struct MyPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
